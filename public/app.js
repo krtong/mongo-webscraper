52c2063fd742c6472d0bb8ea1d372e00502ea2f6
krtong <kevin.r.tong@gmail.com>
2020-03-28 04:28:53 -0700

    working

---
+++ public/app.js
@@ -0,0 +1,88 @@
+// Grab the articles as a json
+$.getJSON("/articles", function (data) {
+  for (var i = 0; i < data.length; i++) {
+    $("#articles").append(`
+      <div class='card'>
+        <div class='card-body'>
+          <p data-id="${data[i]._id}" class="card-title">
+            <a href="${data[i].link}" class='btn-primary-sm'>
+              <h3>${data[i].title}</h3>
+            </a>
+          </p>
+          <button type='button' class='btn btn-primary note-button' id='add-note' data-id="${data[i]._id }">
+            Add a note
+          </button>
+        </div>
+      </div>
+    `);
+  }
+});
+
+$(document).on("click", "#scrape-it", function () {
+  $.get("/scrape", function (data) {
+    console.log(data);
+  }).then(location.reload());
+})
+
+
+// Whenever someone clicks a p tag
+$(document).on("click", "#add-note", async function () {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  const data = await $.ajax({ method: "GET", url: `/articles/${thisId}`})
+  $("#notes").append(`
+  <div class='row'>
+    <div class='col-sm-3'></div>
+    <div class='col-sm-6'>
+    <div class='card'>
+    <div class='card-body'>
+    <div class='card-title'><h4>${data.title}</h4></div>
+    <input id='titleinput' name='title' class='card-body'>
+    <textarea id='bodyinput' name='body'>
+    </textarea>
+    </input>
+    </div>
+    <a href="#" class='btn btn-primary' data-id='${data._id}' id='savenote'>
+    Save Note
+    </a>
+    </div>
+    </div>
+  </div>
+  </br>
+  `);
+
+  if (data.note) {
+    $("#titleinput").val(data.note.title);
+    $("#bodyinput").val(data.note.body);
+  }
+});
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function () {
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+    // With that done
+    .then(function (data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
--- server.js
+++ server.js
@@ -1,10 +1,100 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
 
 //Axios
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-//models
-const db = require("./mmodels");
+//Models
+const db = require("./models");
+
+//Express
+const app = express();
+
+//Middleware:Morgan
+app.use(logger("dev"));
+
+//JsonParse:ReqBody
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+//Public
+app.use(express.static("public"));
+
+//MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+//NewsScraper
+app.get("/scrape", async function (req, {send}) {
+
+  const { data } = await axios.get("https://www.nytimes.com/");
+  const $ = cheerio.load(data);
+
+  $("header h2").each( async function (i, element) {
+    const result = {
+      title: $(this).children("a").text(),
+      link: $(this).children("a").attr("href")
+    };
+    try {
+      const dbArticle = await db.Article.create(result);
+      console.log({dbArticle})
+    }
+    catch(err){
+      console.log(err)
+    }
+    send("Scrape Complete");
+  });
+});
+
+app.get("/articles", async function (req, res) {
+  try {
+    const dbArticle = await db.Article.find({});
+    res.json(dbArticle);
+  }
+  catch(err){ res.json(err) }
+});
+
+app.get("/articles/:id", async function (req, res) {
+  try {
+    const dbArticle = await db.Article.findOne({_id: req.params.id}).populate("note")
+    res.json(dbArticle);
+  }
+  catch(err){
+    res.json(err);
+  }
+});
+
+app.post("/articles/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({
+        _id: req.params.id
+      }, {
+        note: dbNote._id
+      }, {
+        new: true
+      });
+    })
+    .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
